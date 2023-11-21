042a3d6d831f3715b36f00b04270dc12c4fc8e91
snide02 <michaelsnider2002@gmail.com>
2023-11-21 12:33:12 -0500

    Client updated
    Create Client Sockets and Initialized Sockets

--- Client/main.cpp
+++ Client/main.cpp
@@ -3,13 +3,24 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <ws2tcpip.h>
+#include "main.h"
 #pragma comment(lib, "ws2_32.lib")
+
+#define BufferLength 1024
 
 /** declare variable wsa **/
 WSADATA wsa;
+/** declare socket variables – needed for sockets on both client and sever **/
+struct sockaddr_in si_other;
+SOCKET s;
+int slen = sizeof(si_other);
+
+unsigned long noBlock;
+char buffer[BufferLength];
 
 int main() {
 
+    /****** INITIALIZING WINSOCK ***********/
     printf("\n****** INITIALIZING WINSOCK ***********");
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Failed. Error Code : %d", WSAGetLastError());
@@ -17,9 +28,29 @@ int main() {
     }
     else printf("\nWINSOCK INITIALIZED");
 
+    /*****  CREATE CLIENT SOCKET  ****/
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
+        printf("Could not create socket : %d", WSAGetLastError());
+    }
+    printf("UDP CLIENT SOCKEET CREATED.\n");
+
+    /*****  INITIALIZE SOCKET STRUCT   - Non Blocking Client ****/
+    noBlock = 1;
+    ioctlsocket(s, FIONBIO, &noBlock);
+    si_other.sin_addr.s_addr = inet_addr("127.0.0.1");
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(80);
+
+    if (sendto(s, buffer, sizeof(buffer), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR) {
+        printf("sendto() failed with error code : %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    else  printf("\nsent buffer");      
 
 
 
 
     return 0;
 }
+
+
