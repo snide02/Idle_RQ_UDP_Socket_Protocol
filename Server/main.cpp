20af2931bd1dd1f8bbe11d58e45517e20abf641c
snide02 <michaelsnider2002@gmail.com>
2023-11-21 11:58:16 -0500

    Initialize Winsock
    Code from professor in both server and client projects.

---
+++ Server/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+/** declare variable wsa **/
+WSADATA wsa;
+
+int main() {
+
+    printf("\n****** INITIALIZING WINSOCK ***********");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed. Error Code : %d", WSAGetLastError());
+        return 1;
+    }
+    else printf("\nWINSOCK INITIALIZED");
+
+
+
+
+
+    return 0;
+}
